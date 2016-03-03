c329e73963d74b3030ca07f890fba3e6a5aa428f
LehaKos <lehakos@gmail.com>
2016-03-04 00:26:29 +0300

    add temp img loader func

--- app/js/entry.js
+++ app/js/entry.js
@@ -12,4 +12,42 @@ $(function() {
     sliderOpacitySlider.init();
     imgupload.init();
 
+    var $display = $('.main__container-picture-display'),
+        $posInputX = $('.position__input-x'),
+        $posInputY = $('.position__input-y');
+
+    $('.big-img').on('change', function() {
+
+        var $this = $(this),
+            $mainImg = $display.find('.main-img');
+
+        if (!$mainImg.length) {
+            $mainImg = $('<img class="main-img" src="">');
+            $display.append($mainImg);
+        }
+
+        $mainImg.attr('src', $this.val());
+
+    });
+
+    $('.small-img').on('change', function() {
+
+        var $this = $(this),
+            $watermark = $display.find('.watermark');
+
+        if (!$watermark.length) {
+            $watermark = $('<img class="$watermark" src="">');
+            $watermark
+                .css({
+                    'position': 'absolute',
+                    'top': 0,
+                    'left': 0
+                })
+                .appendTo($display);
+        }
+
+        $watermark.attr('src', $this.val());
+
+    });
+
 });
