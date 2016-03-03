72a9409662d2cb71f4a0288c011ac9cbba733959
LehaKos <lehakos@gmail.com>
2016-03-03 23:22:15 +0300

    add position module

--- app/js/entry.js
+++ app/js/entry.js
@@ -3,8 +3,10 @@
 $(function() {
 
     var inputNumberModule = require('./modules/input-number'),
-        sliderOpacitySlider = require('./modules/slider-opacity.js');
+        sliderOpacitySlider = require('./modules/slider-opacity.js'),
+        positionModule = require('./modules/position');
 
+    positionModule.init();
     inputNumberModule.init();
     sliderOpacitySlider.init();
 
