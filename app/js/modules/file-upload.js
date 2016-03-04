21d4f3c855635e3f9d687769241932277ff8defe
Nikita Laitinen <nikitalaitinen@ya.ru>
2016-03-04 16:01:29 +0300

    upload imgs by filereader

--- app/js/modules/file-upload.js
+++ app/js/modules/file-upload.js
@@ -3,60 +3,78 @@ var isImgs = {};
 
 var _setupUpload = function() {
 
-	$input_imgs.on('change',function(e) {
-		_inputChanged($(this));
+	// Изначально все инпуты не верные
+	$input_imgs.each(function(index, el) {
+		isImgs[$(el).attr('id')] = 'false';
 	});
-		
 
-	var _inputChanged = function(img){
 
-		var file_name = img.val().split('\\').pop();
-		var file_format = img.val().split('.').pop();
-		var file_id = img.attr('id');
-		var input_title = img.prevAll('.file__name');
+    $input_imgs.on('change',function(e) {
+        _inputChanged($(this));
+    });
+        
 
-		if ((file_format === 'png') || (file_format === 'jpg')) {
-			console.log("its imgs");
-			input_title.text(file_name);
-			isImgs[file_id] = 'true';
-			_displayImage(img,file_id)
+    var _inputChanged = function(img){
+
+        var file_name = img.val().split('\\').pop();
+        var file_format = img.val().split('.').pop();
+        var file_id = img.attr('id');
+        var input_title = img.prevAll('.file__name');
+
+        if ((file_format === 'png') || (file_format === 'jpg')) {
+            input_title.text(file_name);
+            isImgs[file_id] = 'true';
+            _displayImage(file_id);
+        }
+
+        else {
+            input_title.text("Вы ввели не изображение!!!!");
+            isImgs[file_id] = 'false';
+        }
+    }
+
+    var _displayImage = function (id){
+
+    	var file = document.querySelector('#'+id).files[0];
+    	var reader = new FileReader();
+
+    	if (!$('.wrap_img').length) {
+    		$('.main__container-picture-display').append('<div class="wrap_img"></div>');
+    	}
+
+    	reader.onload = function () {
+			   
+			if ($('img.'+id).length) {
+				$('img.'+id).attr('src',reader.result)
+			}
+			else {
+				$('.wrap_img').append("<img src='" + reader.result + "' class='"+id +"'"  +"/>");
+			}
 		}
 
-		else{
-			input_title.text("Вы ввели не изображение!!!!");
-			console.log("its not images!");
-			isImgs[file_id] = 'false';
-		}
-		console.log(isImgs);
-	}
+		if (file) {
+		    reader.readAsDataURL(file);
+		} 	
+    }  
 
-	var _displayImage = function(img,id){
-		console.log(img,id);
- 		console.log('oke');
-
-		 $('#big_img').fileupload({
-	        done: function (e, data) {
-	            console.log('oke');
-       		}
-    	});
-	}
-
-
-	
-
-	
+    // Функция валидации инпутов
+    var _validate = function(){
+    	for (prop in isImgs){
+    		if (isImgs[prop] === 'false') return false;
+    	}
+		return true;
+    }
 }
-
-
-
-
 
 module.exports = {
 
     init: function() {
-    	if ($input_imgs.length) {
+        if ($input_imgs.length) {
             _setupUpload();
         }
+    },
+    isvalidinput : function(){
+    	_validate();
     }
-
+    
 }
