1ae8239197aa26124a4de447d37b447f8c743529
Jolja06 <elena.an.92@gmail.com>
2016-03-07 16:35:47 +0300

    add module for opacity watermark

--- app/js/modules/slider-opacity.js
+++ app/js/modules/slider-opacity.js
@@ -1,25 +1,46 @@
 var $slider = $('.slider');
 
+//инициализация слайдера 
+//передача значения положения слайдера в input
 var sliderInit = function () {
 
     var rangeSlider = document.getElementById('slider-range');
 
     noUiSlider.create(rangeSlider, {
-        start: [ 60 ],
+        start: [ .1 ],
         range: {
             'min': [  0 ],
-            'max': [ 100 ]
+            'max': [ 1 ]
         },
         connect: 'lower'
     });
 
     var rangeSliderValueElement = document.getElementById('slider-range-value');
 
-    rangeSlider.noUiSlider.on('update', function( values, handle ) {
-        rangeSliderValueElement.value = parseInt(values[handle]);
-    });
+    rangeSlider.noUiSlider.on('update', _opacity);
 
-}
+    function _opacity ( values, handle ) {
+        rangeSliderValueElement.value = values[handle];
+    };
+
+};
+
+//изменение прозрачности
+var $watermarkImg = $('.main__container-picture-display');
+
+
+var watermarkOpacity = function () {
+
+    var rangeSliderOpacity = document.getElementById('slider-range'),
+        img = document.getElementById('img_water_mark');
+
+    rangeSliderOpacity.noUiSlider.on('update', _setOpacity);
+
+    function _setOpacity (values, handle) {
+        img.style.opacity = values[handle];
+    }
+    
+};
 
 module.exports = {
 
@@ -27,6 +48,10 @@ module.exports = {
 
         if ($slider.length) {
             sliderInit();
+        }
+
+        if ($watermarkImg.length) {
+            watermarkOpacity();
         }
 
     }
