ffc31efb7502b479517ce2e05dadb54f6daf9e03
LehaKos <lehakos@gmail.com>
2016-03-09 21:33:00 +0300

    start work on tiling mode

--- app/js/modules/app.js
+++ app/js/modules/app.js
@@ -5,6 +5,7 @@ var $canvas = $('.canvas'),
     $imgWrapper = $('.canvas__inner'),
     $mainImg = $canvas.find('.main-img'),
     $watermark = $canvas.find('.watermark'),
+    $watermarkPosition = $('#watermark-position'),
     $posInputX = $('.position__input_x'),
     $posInputY = $('.position__input_y');
 
@@ -42,35 +43,104 @@ var _dragWatermark = function() {
 
 };
 
+var _createTilingWatermark = function(gutterX, gutterY) {
+
+    var $tiling = $imgWrapper.find('.canvas__tiling'),
+        imgWrapperWidth = $imgWrapper.width(),
+        imgWrapperHeight = $imgWrapper.height(),
+        watermarkWidth = $watermark.width(),
+        watermarkHeight = $watermark.height(),
+        itemsInRow = 0,
+        rows = 0,
+        watermarks = 0,
+        tilingWidth,
+        tilingHeight;
+
+    if (!$tiling.length) {
+        $tiling = $('<div class="canvas__tiling"></div>');
+        $imgWrapper.append($tiling);
+    }
+
+    $tiling.html('');
+
+    itemsInRow = Math.floor(imgWrapperWidth / watermarkWidth);
+    rows = Math.floor(imgWrapperHeight / watermarkHeight);
+    watermarks = itemsInRow * rows;
+    
+    $tiling.width((watermarkWidth + gutterX) * itemsInRow);
+    $tiling.height((watermarkHeight + gutterY) * rows);
+
+    for (var i = 0; i < watermarks; i++) {
+
+        var $watermarkClone = $watermark.clone();
+
+        $watermarkClone.css({
+            'position': 'static',
+            'margin-left': gutterX,
+            'margin-top': gutterY
+        });
+
+        $tiling.append($watermarkClone);
+
+    }
+
+};
+
+var _onModeChange = function() {
+
+    var oldLimitX,
+        oldLimitY;
+
+    $watermarkPosition.on('positionModeChange', function(e) {
+
+        var newMode = $watermarkPosition.attr('data-mode');
+
+        if (newMode === 'tiling') {
+            $watermark.css({
+                'top': 0,
+                'left': 0
+            });
+
+            oldLimitX = parseInt($posInputX.attr('data-max'));
+            oldLimitY = parseInt($posInputY.attr('data-max'));
+
+            inputNumberModule.setLimit($posInputX, 'max', 100);
+            inputNumberModule.setLimit($posInputY, 'max', 100);
+
+            _createTilingWatermark(10, 10);
+
+        } else if (newMode === 'single') {
+
+            inputNumberModule.setLimit($posInputX, 'max', oldLimitX);
+            inputNumberModule.setLimit($posInputY, 'max', oldLimitY);
+
+            var curCoord = positionModule.getGridCoord($('#watermark-position'));
+            positionModule.setPosition($('#watermark-position'), curCoord.x, curCoord.y);
+        }
+
+    });
+
+};
+
 var _changeWatermarkPosition = function() {
 
-    positionModule.setInputChangeCallback(function($block, $input) {
+    var $watermarkInputs = $watermarkPosition.find('.position__input');
 
-        if ($block.is('#watermark-position')) {
+    $watermarkInputs.on('change', function(e) {
 
-            var val = parseInt($input.val());
+        var $this = $(this),
+            val = parseInt($this.val());
 
-            if ($block.attr('data-mode') === 'single') {
+        if ($watermarkPosition.attr('data-mode') === 'single') {
 
-                if ($input.is($posInputX)) {
-                    $watermark.css('left', val + 'px');
+            if ($this.is($posInputX)) {
+                $watermark.css('left', val + 'px');
 
-                } else if ($input.is($posInputY)) {
+            } else if ($this.is($posInputY)) {
 
-                    $watermark.css('top', val + 'px');
-                }
-
-            } else if ($block.attr('data-mode') === 'tiling') {
-
-                if ($input.is($posInputX)) {
-                    $watermark.css('left', val + 'px');
-
-                } else if ($input.is($posInputY)) {
-
-                    $watermark.css('top', val + 'px');
-                }
-
+                $watermark.css('top', val + 'px');
             }
+
         }
 
     });
@@ -117,5 +187,6 @@ module.exports = {
     init: function() {
         _changeWatermarkPosition();
         _dragWatermark();
+        _onModeChange();
     }
 }
